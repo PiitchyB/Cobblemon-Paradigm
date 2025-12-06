4c7520c7f7eb2091a85004070932db25e87b05e0
Piitch <pikminator8@gmail.com>
2025-12-06 00:08:13 -0500

    Implemented Crown Jewels and Slag Ray
    - Added the JavaScript for Crown Jewels and Slag Ray
    - Added the language data for Crown Jewels and Slag Ray

---
+++ data/cobblemon/abilities/crownjewels.js
@@ -0,0 +1,36 @@
+{
+    onAnyAfterBoost(boost, target, source, effect) {
+      if (source !== this.effectState.target) return;
+      if (!target || target === source) return;
+      let statsLowered = false;
+      let i;
+      for (i in boost) {
+        if (boost[i] < 0) {
+          statsLowered = true;
+        }
+      }
+      if (!statsLowered) return;
+      const heldGemType = {
+        firegem: "brn",
+        icegem: "haze",
+        poisongem: "psn",
+        electricgem: "par",
+        normalgem: "yawn",
+        grassgem: "leechseed"
+      };
+      if (!heldGemType[source.getItem().id]) return;
+      if (["brn", "par", "psn"].includes(heldGemType[source.getItem().id])) {
+        target.trySetStatus(heldGemType[source.getItem().id], source, effect);
+      } else if (heldGemType[source.getItem().id] === "haze") {
+        for (const pokemon of this.getAllActive()) {
+          pokemon.clearBoosts();
+          this.add("-clearallboost", pokemon);
+        }
+      } else {
+        target.addVolatile(heldGemType[source.getItem().id], source, effect);
+      }
+      source.useItem();
+    },
+    flags: { breakable: 1 },
+    name: "Crown Jewels"
+}
---
+++ data/cobblemon/moves/slagray.js
@@ -0,0 +1,19 @@
+{
+    accuracy: 100,
+    basePower: 80,
+    category: "Special",
+    overrideOffensiveStat: "atk",
+    name: "Slag Ray",
+    pp: 15,
+    priority: 0,
+    flags: { protect: 1, mirror: 1, metronome: 1 },
+    secondary: {
+      chance: 100,
+      boosts: {
+        def: -1
+      }
+    },
+    target: "normal",
+    type: "Fire",
+    contestType: "Cool"
+}
