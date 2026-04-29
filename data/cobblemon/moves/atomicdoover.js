de2e8cb5805627841d0ddc9426082356e897ca85
Piitch <pikminator8@gmail.com>
2026-04-29 15:54:35 -0400

    Added Atomic-Do-Over
    - Added Atomic-Do-Over to Minior's learnset at level 55

---
+++ data/cobblemon/moves/atomicdoover.js
@@ -0,0 +1,21 @@
+{
+    num: 9002,
+    accuracy: true,
+    basePower: 0,
+    category: "Status",
+    name: "Atomic-Do-Over",
+    pp: 15,
+    priority: 0,
+    flags: { snatch: 1, metronome: 1 },
+    boosts: {
+      def: -3,
+      spd: -3,
+      spa: 3,
+      spe: 3
+    },
+    secondary: null,
+    target: "self",
+    type: "Psychic",
+    zMove: { effect: "clearnegativeboost" },
+    contestType: "Cool"
+}
