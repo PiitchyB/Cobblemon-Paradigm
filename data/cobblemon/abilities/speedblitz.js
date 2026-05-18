731949fb44a092b1e5443e3415fa2843668a80f1
Piitch <pikminator8@gmail.com>
2026-05-18 01:37:34 -0400

    Animation revamps galore
    - Retooled Chunlong's idle, moving, fainting, and cry animations
    - Gave Chunlong an idle quirk
    - Gave Chunlong the ability to wear hat and face items
    - Gave Salaberrant a recoil animation
    - Fixed a logic error in the Speed Blitz ability
    - Gave Chunlong the ability to look around

--- data/cobblemon/abilities/speedblitz.js
+++ data/cobblemon/abilities/speedblitz.js
@@ -1,14 +1,14 @@
 {
     onBasePower(basepower, attacker, defender, move) {
-      let ratio = Math.floor(attacker.getStat("spe") / defender.getStat("spe"));
-      if (!isFinite(ratio)) ratio = 0;
+      let ratio = Math.floor((attacker.getStat("spe") / defender.getStat("spe") - 1) / 0.2);
+      if (!isFinite(ratio) || ratio < 0) ratio = 0;
       const boost = [1, 1.1, 1.2, 1.3, 1.4, 1.5][Math.min(ratio, 5)];
       this.debug(`speed blitz boost: ${boost}`);
       return this.chainModify(boost);
     },
     onSourceModifyDamage(damage, source, target, move) {
-      let ratio = Math.floor(target.getStat("spe") / source.getStat("spe"));
-      if (!isFinite(ratio)) ratio = 0;
+      let ratio = Math.floor((target.getStat("spe") / source.getStat("spe") - 1) / 0.2);
+      if (!isFinite(ratio) || ratio < 0) ratio = 0;
       const drop = [1, 1.1, 1.2, 1.3, 1.4, 1.5][Math.min(ratio, 5)];
       this.debug(`speed blitz defense drop: ${drop}`);
       return this.chainModify(drop);
